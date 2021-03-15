12775354c3cabde18b9f4b49a3356ada71af62a2
DivyaPatange <divyapatange0@gmail.com>
2021-03-15 10:16:11 +0530

    auth changes

--- config/auth.php
+++ config/auth.php
@@ -80,6 +80,7 @@ return [
             'driver' => 'eloquent',
             'model' => App\Models\Admin::class,
         ],
+        
 
         // 'users' => [
         //     'driver' => 'database',
